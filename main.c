94f979e03292c49e6d508dc4b3689bfd3481eacf
blah <blah@blah.com>
2018-11-18 06:53:33 -0800

    Changes to print all the fields in the ibv_device structure.

--- main.c
+++ main.c
@@ -16,9 +16,13 @@ main(int argc, char *argv[])
 
 	/* Let's print some basic info. */
 	for (i=0; i < n; i++) {
-		printf ("%d: %s %llx\n", i,
-			ibv_get_device_name(dev_list[i]),
-			(unsigned long long) be64toh(ibv_get_device_guid(dev_list[i])));
+		printf ("%d: %s/%s %llx [%s,%d] [%d] [%s] [%s]\n", i,
+			dev_list[i]->dev_name, dev_list[i]->name,
+			(unsigned long long) be64toh(ibv_get_device_guid(dev_list[i])),
+			ibv_node_type_str(dev_list[i]->node_type), dev_list[i]->node_type,
+			dev_list[i]->transport_type,
+			dev_list[i]->dev_path,
+			dev_list[i]->ibdev_path);
 	}
 
 	/* Free the link list. */
