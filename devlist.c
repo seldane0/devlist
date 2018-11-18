910a292c8cfdd6602c7dba3d5076f7fff32ef5df
blah <blah@blah.com>
2018-11-18 05:16:18 -0800

    Added call to free dev list.

--- devlist.c
+++ devlist.c
@@ -14,10 +14,15 @@ main(int argc, char *argv[])
 		return -1;
 	}
 
+	/* Let's print some basic info. */
 	for (i=0; i < n; i++) {
 		printf ("%d: %s %llx\n", i,
 			ibv_get_device_name(dev_list[i]),
 			(unsigned long long) be64toh(ibv_get_device_guid(dev_list[i])));
 	}
+
+	/* Free the link list. */
+	ibv_free_device_list(dev_list);
+
 	return 0;
 }
